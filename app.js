0206caa13cb7a157a6021dd354288d10850c86dd
Ddevyash <vandiyashira@gmail.com>
2024-06-18 00:51:21 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+
+
 let toggle = document.querySelector('.hamburger');
     toggle.addEventListener('click', function (){
     document.querySelector('.nav-menu').style.display='block';
+    document.querySelector('.cancel img').style.display='block';
     this.style.display='none';
+})
+
+let closeBtn= document.querySelector('.cancel');
+    closeBtn.addEventListener('click', function(){
+    document.querySelector('.nav-menu').style.display='none';
+    document.querySelector('.hamburger').style.display='block';
+    this.style.display='none';
+
 })
